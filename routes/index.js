f493043bbc5e1c62be8dad62d9b4709aaa4ac1dc
Hakall <abouskine@gmail.com>
2016-03-18 15:21:19 +0100

    starting manager interface

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,66 @@
 var express = require('express');
 var router = express.Router();
-var CASAuthentication = require('cas-authentication');
 var mongoose = require(process.cwd() + '/controllers/mongoose');
+// var passport = require('passport');
+
 var UserModel;
 
-mongoose.initialize(function(){
-	UserModel = mongoose.UserModel;
+mongoose.initialize(function() {
+    UserModel = mongoose.UserModel;
 });
 
-var cas = new CASAuthentication({
-    cas_url     : 'https://tequila:8443/cas',
-    service_url : 'https://tequila:4443'
+// passport.use(new(require('passport-cas').Strategy)({
+//     version: 'CAS3.0',
+//     ssoBaseURL: 'https://tequila:8443/cas',
+//     serverBaseURL: 'https://tequila:4443/'
+// }, function(login, done) {
+//     UserModel.findOne({ uid: login }, function(err, user) {
+//         if (err) {
+//         	console.log(err);
+//             return done(err);
+//         }
+//         if (!user) {
+//         	console.log('Unknown user');
+//             return done(null, false, { message: 'Unknown user' });
+//         }
+//         user.attributes = profile.attributes;
+//         console.log(user);
+//         return done(null, user);
+//     });
+// }));
+
+
+router.get('/', function(req, res) {
+    res.render('index', { title: 'Esup Otp Manager' });
 });
 
-/* GET home page. */
-router.get('/', cas.bounce, function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/preferences', function(req, res) {
+    res.render('dashboard', { title: 'Esup Otp Manager : Preferences' });
 });
 
-// Unauthenticated clients will receive a 401 Unauthorized response instead of 
-// the JSON data. 
-router.get( '/preferences', cas.block, function ( req, res ) {
-    res.render('preferences', { title: 'Preferences' });
-});
- 
-// An example of accessing the CAS user session variable. This could be used to 
-// retrieve your own local user records based on authenticated CAS username. 
-router.get( '/admin', cas.block, function ( req, res ) {
-    res.render('preferences', { title: 'Administration' });
-});
- 
-// This route will de-authenticate the client with the Express server and then 
-// redirect the client to the CAS logout page. 
-router.get( '/logout', cas.logout );
+// router.get('/login', function(req, res, next) {
+//     passport.authenticate('cas', function(err, user, info) {
+//         if (err) {
+//             console.log(err);
+//             return next(err);
+//         }
+
+//         if (!user) {
+//             // req.session.messages = info.message;
+//             console.log(info.message);
+//             return res.redirect('/');
+//         }
+
+//         req.logIn(user, function(err) {
+//             if (err) {
+//             	console.log(err);
+//                 return next(err);
+//             }
+//             console.log(user);
+//             req.session.messages = '';
+//             return res.redirect('/preferences');
+//         });
+//     })(req, res, next);
+// });
 
 module.exports = router;

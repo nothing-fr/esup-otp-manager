5dcae14042a623e43610e83bbffc576686bedcc3
Hakall <abouskine@gmail.com>
2016-04-25 17:11:47 +0200

    change hash generation

--- services/utils.js
+++ services/utils.js
@@ -3,7 +3,7 @@ var CryptoJS = require('crypto-js');
 
 exports.get_hash = function(uid) {
     var d = new Date();
-    var present_salt = d.getDay().toString()+d.getHours().toString();
+    var present_salt = d.getUTCDate().toString()+d.getHours().toString();
     present_hash = CryptoJS.SHA256(CryptoJS.MD5(properties.esup.users_secret).toString()+uid+present_salt).toString();  
     return present_hash;
 }

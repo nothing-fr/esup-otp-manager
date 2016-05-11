d57583b0ba86c97646c83e9b2b8f87f93f88becc
Hakall <abouskine@gmail.com>
2016-05-11 09:23:02 +0200

    use process.env.port

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ var properties = require(__dirname + '/../properties/properties');
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(properties.esup.port || process.env.PORT || '4000');
+var port = normalizePort(process.env.PORT || '4000');
 app.set('port', port);
 
 /**

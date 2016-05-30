ab9bc7c14d3426e15597c7c30ba31d8f02e85f32
Hakall <abouskine@gmail.com>
2016-05-30 15:40:19 +0200

    resolve warnings

--- server/app.js
+++ server/app.js
@@ -6,12 +6,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
-var properties = require(__dirname+'/../properties/properties');
+var properties = require(__dirname + '/../properties/properties');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname+'/..', 'views'));
+app.set('views', path.join(__dirname + '/..', 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -20,13 +20,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname+'/..', 'public')));
+app.use(express.static(path.join(__dirname + '/..', 'public')));
 
-app.use(expressSession({secret: properties.esup.session_secret_key}));
+app.use(expressSession({
+    secret: properties.esup.session_secret_key,
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(passport.initialize());
-app.use(passport.session()); 
+app.use(passport.session());
 
-app.use(function(req,res,next){
+app.use(function(req, res, next) {
     res.locals.session = req.session;
     next();
 });
@@ -36,9 +40,9 @@ app.use('/', routes(passport));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -46,23 +50,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 

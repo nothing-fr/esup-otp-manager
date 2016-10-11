247f199fbb44c579d7ffdc2cffa64a38eb3c3715
Hakall <abouskine@gmail.com>
2016-10-11 09:44:16 +0200

    resolve bug firefox with jquery init

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -6,7 +6,7 @@
             $('.collapsible').collapsible({
                 accordion: false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
             });
-        },1);
+        },100);
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
